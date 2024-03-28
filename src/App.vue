52da4fb44d3ce838c77f1f615a10b56f75285003
MarcoFonti <theemporiumg@gmail.com>
2024-03-28 20:16:41 +0100

    add request API

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,37 @@
 <!-- JS -->
 <script>
+/* IMPORTO APPHEADER */
 import AppHeader from './components/AppHeader.vue';
+/* IMPORTO AXIOS */
+import axios from 'axios';
+/* URL ENDPOINT */
+const endpoint = 'http://127.0.0.1:8000/api/projects/'; 
 
 export default {
     name: 'App',
+    data: () => ({
+        projects: []
+    }),
+
     /* COMPONENTI */
-    components: { AppHeader }
+    components: { AppHeader },
+
+    /* METODI */
+    methods:{
+        /* FUNZIONE PER RICHIEDERE API */
+        apiProjects(){
+            axios.get(endpoint).then((res) => {
+                this.projects = res.data;
+            })
+        }
+    },
+
+    /* ALL'AVVIO DELLA PAGINA */
+    created() {
+        /* RIESTA FUNZIONE API */
+        this.apiProjects();
+    }
+
 
 };
 
