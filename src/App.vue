b73952449d4dc2fdb87dba03ab49511586148346
MarcoFonti <theemporiumg@gmail.com>
2024-03-28 18:30:23 +0100

    add scaffolding

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<!-- JS -->
+<script>
+
+export default {
+    name: 'App',
+    
+};
+
+</script>
+
+<!-- HTML -->
+<template>
+    <main class="container">
+        <h1>Pippo</h1>
+    </main>
+</template>
+
+<!-- CSS -->
+<style scoped>
+
+</style>
